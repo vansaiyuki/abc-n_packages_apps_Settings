LineageOS/android_packages_apps_Settings

Add setup UI for minimum delay between an app's notification sounds.
Change-Id: I70e9cc308b4f7c7681457a0b09dd73e5d1aebb73

src/com/android/settings/notification/NotificationBackend.java
@@ -47,6 +47,7 @@ public class NotificationBackend {
         row.priority = getHighPriority(row.pkg, row.uid);
         row.peekable = getPeekable(row.pkg, row.uid);
         row.sensitive = getSensitive(row.pkg, row.uid);
+        row.soundTimeout = getNotificationSoundTimeout(row.pkg, row.uid);
         return row;
     }
 
@@ -149,6 +150,25 @@ public class NotificationBackend {
         }
     }
 
+    public long getNotificationSoundTimeout(String pkg, int uid) {
+        try {
+            return sINM.getPackageNotificationSoundTimeout(pkg, uid);
+        } catch (Exception e) {
+            Log.w(TAG, "Error calling NoMan", e);
+            return 0;
+        }
+    }
+
+    public boolean setNotificationSoundTimeout(String pkg, int uid, long timeout) {
+        try {
+            sINM.setPackageNotificationSoundTimeout(pkg, uid, timeout);
+            return true;
+        } catch (Exception e) {
+            Log.w(TAG, "Error calling NoMan", e);
+            return false;
+        }
+    }
+
     static class Row {
         public String section;
     }
@@ -164,6 +184,7 @@ public class NotificationBackend {
         public boolean peekable;
         public boolean sensitive;
         public boolean first;  // first app in section
+        public long soundTimeout;
     }
 
 }

vansaiyuki/abc-n_packages_apps_Settings

src/com/android/settings/notification/NotificationBackend.java
@@ -52,6 +52,7 @@ public class NotificationBackend {
         row.appImportance = getImportance(row.pkg, row.uid);
         row.appBypassDnd = getBypassZenMode(row.pkg, row.uid);
         row.appVisOverride = getVisibilityOverride(row.pkg, row.uid);
+        row.soundTimeout = getNotificationSoundTimeout(row.pkg, row.uid);
         row.lockScreenSecure = new LockPatternUtils(context).isSecure(
                 UserHandle.myUserId());
         return row;
@@ -131,6 +132,25 @@ public class NotificationBackend {
         }
     }
 
+    public long getNotificationSoundTimeout(String pkg, int uid) {
+        try {
+            return sINM.getNotificationSoundTimeout(pkg, uid);
+        } catch (Exception e) {
+            Log.w(TAG, "Error calling NoMan", e);
+            return 0;
+        }
+    }
+
+    public boolean setNotificationSoundTimeout(String pkg, int uid, long timeout) {
+        try {
+            sINM.setNotificationSoundTimeout(pkg, uid, timeout);
+            return true;
+        } catch (Exception e) {
+            Log.w(TAG, "Error calling NoMan", e);
+            return false;
+        }
+    }
+
     static class Row {
         public String section;
     }
@@ -147,6 +167,7 @@ public class NotificationBackend {
         public int appImportance;
         public boolean appBypassDnd;
         public int appVisOverride;
+        public long soundTimeout;
         public boolean lockScreenSecure;
     }
 }