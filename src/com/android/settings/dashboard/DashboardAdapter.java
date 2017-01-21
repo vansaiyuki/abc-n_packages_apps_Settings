ce69b604e23d6c5fec449aa7384518a2cdc7b187
Alex Cruz <mazdarider23@gmail.com>
2017-01-21 16:56:05 +0100

    Dashboard summaries double lines toggle [2/2]
    Inspired by @xWASABI and his commit here
    TeamTwisted/packages_apps_Settings@3c62ed2
    
    This would come in handy for tablets that use 2 rows in the dashboard and for when
    translations are just too big to fit in 1 line or bigger DPI
    
    - Removed background color on the main dashboard, looks better when applying
    2/3 columns and does not break any themes so win win
    
    Change-Id: I5cf36cb619bbfa0e1e26fc190957dfb595854f86

--- src/com/android/settings/dashboard/DashboardAdapter.java
+++ src/com/android/settings/dashboard/DashboardAdapter.java
@@ -20,6 +20,7 @@ import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.Icon;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
@@ -278,7 +279,7 @@ public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.Dash
 
     private void showRemoveOption(View v, final Tile suggestion) {
         PopupMenu popup = new PopupMenu(
-                new ContextThemeWrapper(mContext, R.style.Theme_AppCompat_DayNight), v);
+                 new ContextThemeWrapper(mContext, R.style.Theme_AppCompat_DayNight), v);
         popup.getMenu().add(R.string.suggestion_remove).setOnMenuItemClickListener(
                 new MenuItem.OnMenuItemClickListener() {
             @Override
@@ -355,6 +356,12 @@ public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.Dash
         if (!TextUtils.isEmpty(tile.summary)) {
             holder.summary.setText(tile.summary);
             holder.summary.setVisibility(View.VISIBLE);
+            if (Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.DASHBOARD_SUMMARY_DOUBLE_LINES, 0) == 1) {
+                holder.summary.setSingleLine(false);
+            } else {
+                holder.summary.setSingleLine(true);
+            }
         } else {
             holder.summary.setVisibility(View.GONE);
         }
